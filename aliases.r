c23dd891ef0ab00f97c4a1b5081678b9fdaad2db
Jamie <jamie.cummins@ugent.be>
2019-10-01 11:47:13 +0200

    added functions

--- aliases.r
+++ aliases.r
@@ -2,5 +2,12 @@
 # (will move to a package in time)
 
 seize_mean <- mean
+unionise <- dplyr::left_join()
+divide_classes <- dplyr::right_join()
+redistribute <- tidyr::spread()
+hoard <- tidyr::gather()
+gulag_nas <- na.omit()
+gulag <- dplyr::filter() # ideally we actually want this to have the opposite effect on the arguments in filter - i.e., you say gulag X, and it removes all X
 
-# exclusions should be send_to_gulag. maybe alias schorsch package function?
+# exclusions should be send_to_gulag. maybe alias schorsch package function
+# had the same thought above - could an inverse of filter also work?
